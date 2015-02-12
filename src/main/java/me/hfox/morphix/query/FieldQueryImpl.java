de96c7ee52346708e1b829c346c8401206cce2b2
Harry Fox <harry.fox.ic@icloud.com>
2015-02-12 17:47:15 +0000

    Use morphix field not method

--- src/main/java/me/hfox/morphix/query/FieldQueryImpl.java
+++ src/main/java/me/hfox/morphix/query/FieldQueryImpl.java
@@ -98,7 +98,7 @@ public class FieldQueryImpl<T> implements FieldQuery<T> {
                 continue;
             }
 
-            FieldMapper mapper = FieldMapper.create(object.getClass(), null, query.getMorphix());
+            FieldMapper mapper = FieldMapper.create(object.getClass(), null, morphix);
             list.add(mapper.marshal(object));
         }
 
