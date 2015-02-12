34163314ec96365e73c3164b95ef978fce791ca5
Harry Fox <harry.fox.ic@icloud.com>
2015-02-12 21:05:15 +0000

    Change default value of dbRef to false

--- src/main/java/me/hfox/morphix/annotation/Reference.java
+++ src/main/java/me/hfox/morphix/annotation/Reference.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Reference {
 
-    public boolean dbRef() default true;
+    public boolean dbRef() default false;
 
 }
