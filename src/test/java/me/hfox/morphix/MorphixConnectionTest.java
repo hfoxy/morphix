588053156c55e5294b6e181a5efb28c250a67b5c
Harry Fox <harry.fox.ic@icloud.com>
2015-02-12 21:32:27 +0000

    Just catch the Timeout exception

--- src/test/java/me/hfox/morphix/MorphixConnectionTest.java
+++ src/test/java/me/hfox/morphix/MorphixConnectionTest.java
@@ -1,6 +1,7 @@
 package me.hfox.morphix;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoTimeoutException;
 import junit.framework.TestCase;
 import me.hfox.morphix.entities.Address;
 import me.hfox.morphix.entities.User;
@@ -10,34 +11,28 @@ import java.util.Arrays;
 public class MorphixConnectionTest extends TestCase {
 
     private Morphix morphix;
-    private boolean cont;
 
     @Override
     protected void setUp() throws Exception {
         MongoClient client = new MongoClient("127.0.0.1", 27017);
         morphix = new Morphix(client, "morphix_testing");
-
-        try {
-            morphix.getDatabase().getCollection("users").find();
-            cont = true;
-        } catch (Exception ex) {
-            System.out.println("Could not connect to MongoDB server - ignoring tests which require DB connection");
-        }
     }
 
     public void testUser() throws Exception {
-        if (!cont) return;
+        try {
+            Address address = new Address(Arrays.asList("some", "cool", "address"), "post code");
+            morphix.store(address);
 
-        Address address = new Address(Arrays.asList("some", "cool", "address"), "post code");
-        morphix.store(address);
+            User user = new User("email", "username", "password", address);
+            System.out.println(user);
+            morphix.store(user);
 
-        User user = new User("email", "username", "password", address);
-        System.out.println(user);
-        morphix.store(user);
-
-        morphix.getCache(User.class).clear();
-        User queried = morphix.createQuery(User.class).field("email").equal("email").get();
-        System.out.println(queried);
+            morphix.getCache(User.class).clear();
+            User queried = morphix.createQuery(User.class).field("email").equal("email").get();
+            System.out.println(queried);
+        } catch (MongoTimeoutException ex) {
+            System.out.println("Could not connect to MongoDB server - ignoring tests which require DB connection");
+        }
     }
 
 }
