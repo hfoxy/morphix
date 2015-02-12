a9a7089f6698d54ff08ad4c7b12937e4f4549f11
Harry Fox <harry.fox.ic@icloud.com>
2015-02-12 21:25:23 +0000

    Ignore tests that require a DB if no DB is found

--- src/test/java/me/hfox/morphix/MorphixConnectionTest.java
+++ src/test/java/me/hfox/morphix/MorphixConnectionTest.java
@@ -10,14 +10,24 @@ import java.util.Arrays;
 public class MorphixConnectionTest extends TestCase {
 
     private Morphix morphix;
+    private boolean cont;
 
     @Override
     protected void setUp() throws Exception {
         MongoClient client = new MongoClient("127.0.0.1", 27017);
         morphix = new Morphix(client, "morphix_testing");
+
+        try {
+            morphix.getDatabase().getCollection("users");
+            cont = true;
+        } catch (Exception ex) {
+            System.out.println("Could not connect to MongoDB server - ignoring tests which require DB connection");
+        }
     }
 
     public void testUser() throws Exception {
+        if (!cont) return;
+
         Address address = new Address(Arrays.asList("some", "cool", "address"), "post code");
         morphix.store(address);
 
