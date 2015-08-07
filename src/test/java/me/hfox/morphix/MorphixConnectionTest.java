a14a0452ee5a5cf0e65da761f537dda9e5b7c0a0
Harry Fox <harry.fox.ic@icloud.com>
2015-08-07 22:29:11 +0100

    Use Assumptions to check if the connection test should be executed or not

--- src/test/java/me/hfox/morphix/MorphixConnectionTest.java
+++ src/test/java/me/hfox/morphix/MorphixConnectionTest.java
@@ -1,58 +1,70 @@
 package me.hfox.morphix;
 
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
 import com.mongodb.MongoTimeoutException;
-import junit.framework.TestCase;
+import com.mongodb.ServerAddress;
 import me.hfox.morphix.entities.Address;
 import me.hfox.morphix.entities.User;
 import me.hfox.morphix.query.Query;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
-public class MorphixConnectionTest extends TestCase {
+import static org.junit.Assert.*;
+import static org.junit.Assume.assumeTrue;
 
-    private Morphix morphix;
+public class MorphixConnectionTest {
 
-    @Override
-    protected void setUp() throws Exception {
-        MongoClient client = new MongoClient("127.0.0.1", 27017);
+    private static Morphix morphix;
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        MongoClient client = new MongoClient(new ServerAddress("127.0.0.1", 27017), MongoClientOptions.builder().connectTimeout(2500).build());
         morphix = new Morphix(client, "morphix_testing");
+
+        try {
+            List<String> names = client.getDatabaseNames();
+            System.out.println("Connected to DB: " + names);
+        } catch (MongoTimeoutException ex) {
+            assumeTrue("Could not connect to MongoDB server - ignoring tests which require DB connection", false);
+        }
     }
 
+    @Test
     public void testUser() throws Exception {
-        try {
-            Address address = new Address(Arrays.asList("some", "cool", "address"), "post code");
-            morphix.store(address);
+        Address address = new Address(Arrays.asList("some", "cool", "address"), "post code");
+        morphix.store(address);
+        assumeTrue(true);
 
-            User user = new User("email", "username", "password", address);
-            morphix.store(user); // create
+        User user = new User("email", "username", "password", address);
+        morphix.store(user); // create
 
-            user.setUsername("testing");
-            morphix.store(user); // save
-            // System.out.println(user.getId());
+        user.setUsername("testing");
+        morphix.store(user); // save
+        // System.out.println(user.getId());
 
-            Query<User> query = morphix.createQuery(User.class).field("_id").equal(user.getId());
-            User queried = query.get();
-            assertSame(user, queried);
+        Query<User> query = morphix.createQuery(User.class).field("_id").equal(user.getId());
+        User queried = query.get();
+        assertSame(user, queried);
 
-            morphix.getCache(User.class).clear();
+        morphix.getCache(User.class).clear();
 
-            query = morphix.createQuery(User.class).field("_id").equal(user.getId());
-            // System.out.println(query.toQueryObject());
-            queried = query.get();
-            // System.out.println(queried.getId());
+        query = morphix.createQuery(User.class).field("_id").equal(user.getId());
+        // System.out.println(query.toQueryObject());
+        queried = query.get();
+        // System.out.println(queried.getId());
 
-            assertEquals(user, queried);
+        assertEquals(user, queried);
 
-            query = morphix.createQuery(User.class).field("_id").equal(user.getId());
-            query.delete();
+        query = morphix.createQuery(User.class).field("_id").equal(user.getId());
+        query.delete();
 
-            assertNull(queried.getPassword());
-            queried = query.get();
-            assertNull(queried);
-        } catch (MongoTimeoutException ex) {
-            System.out.println("Could not connect to MongoDB server - ignoring tests which require DB connection");
-        }
+        assertNull(queried.getPassword());
+        queried = query.get();
+        assertNull(queried);
     }
 
 }
