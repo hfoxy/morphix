6b6fe9066c45187dacbc53de581ea18876a9ffab
Harry Fox <harry.fox.ic@icloud.com>
2017-11-28 10:47:25 +0000

    Add documentation to MorphixConnector

--- morphix/src/main/java/me/hfox/morphix/connector/MorphixConnector.java
+++ morphix/src/main/java/me/hfox/morphix/connector/MorphixConnector.java
@@ -9,12 +9,31 @@ import me.hfox.morphix.query.Query;
  */
 public interface MorphixConnector {
 
+    /**
+     * Connect to the database
+     */
     void connect();
 
+    /**
+     * Disconnect from the database
+     */
     void disconnect();
 
+    /**
+     * Query the database using the specified class as a collection and result reference
+     * @param cls The expected resulting class, also used to find the collection
+     * @param <T> The expected resulting type
+     * @return A query based on the given class
+     */
     <T> Query<T> createQuery(Class<T> cls);
 
+    /**
+     * Query the given collection with the specified class as an expected result
+     * @param cls The expected resulting class
+     * @param collection The collection to query
+     * @param <T> The expected resulting type
+     * @return A query based on the given class
+     */
     <T> Query<T> createQuery(Class<T> cls, String collection);
 
 }
