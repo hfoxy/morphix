451606f419e3b2ebf651ceaa3c90515bc25efac8
Harry Fox <harry.fox.ic@icloud.com>
2018-02-16 23:44:02 +0000

    Decide on alternate method of running tests for this class

---
+++ morphix-mongo/src/test/java/uk/hfox/morphix/mongo/entity/MongoEntityManagerTest.java
@@ -0,0 +1,14 @@
+package uk.hfox.morphix.mongo.entity;
+
+import org.junit.jupiter.api.Test;
+import uk.hfox.morphix.connector.TestConnector;
+
+class MongoEntityManagerTest {
+
+    @Test
+    void getConnector() {
+        TestConnector connector = new TestConnector();
+        // MongoEntityManager manager = new MongoEntityManager(connector);
+    }
+
+}
