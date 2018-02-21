ea4cc07b9c635e3c7cb1242aca8a1126496e04d3
Harry Fox <harry.fox.ic@icloud.com>
2018-02-21 09:52:39 +0000

    Add test for default converter methods

---
+++ morphix-api/src/test/java/uk/hfox/morphix/transform/ConverterTest.java
@@ -0,0 +1,55 @@
+package uk.hfox.morphix.transform;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class ConverterTest {
+
+    @Test
+    void pullBasic() {
+        BasicConverter converter = new BasicConverter();
+        converter.pull("key", new Object());
+        converter.pull("key", new Object(), null);
+    }
+
+    @Test
+    void pullValue() {
+        ValueConverter converter = new ValueConverter();
+        converter.pull("key", new Object());
+        converter.pull("key", new Object(), null);
+    }
+
+    private static class BasicConverter implements Converter<Object> {
+
+        @Override
+        public Object pull(String key, Object entry) {
+            assertNotNull(key);
+            assertNotNull(entry);
+            return null;
+        }
+
+        @Override
+        public void push(String key, Object entry, Object value) {
+            // not interested
+        }
+    }
+
+    private static class ValueConverter implements Converter<Object> {
+
+        @Override
+        public Object pull(String key, Object entry, Object value) {
+            assertNotNull(key);
+            assertNotNull(entry);
+            assertNull(value);
+            return null;
+        }
+
+        @Override
+        public void push(String key, Object entry, Object value) {
+            // not interested
+        }
+    }
+
+}
