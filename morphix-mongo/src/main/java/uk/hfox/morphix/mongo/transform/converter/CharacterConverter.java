df3a838fb6e2684ff0e2b3ba62ad9d41fba42da2
Harry Fox <harry.fox.ic@icloud.com>
2018-02-21 09:42:39 +0000

    Mongo stores a wider variety of types than expected

--- morphix-mongo/src/main/java/uk/hfox/morphix/mongo/transform/converter/CharacterConverter.java
+++ morphix-mongo/src/main/java/uk/hfox/morphix/mongo/transform/converter/CharacterConverter.java
@@ -25,16 +25,12 @@ public class CharacterConverter implements Converter<Document> {
 
     @Override
     public Character pull(String key, Document entry) {
-        return entry.getString(key).charAt(0);
+        return (Character) entry.get(key);
     }
 
     @Override
     public void push(String key, Document entry, Object value) {
-        if (!(value instanceof Character)) {
-            throw new IllegalArgumentException("value must be character");
-        }
-
-        entry.put(key, value.toString());
+        entry.put(key, value);
     }
 
 }
